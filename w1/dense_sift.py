f3265cdfec1652ca95893b439e2cdc1a7014b7e8
Jorge López <jorge.lopez@neosentec.com>
2018-12-12 17:41:25 +0100

    Added random scale to dense sift

--- w1/dense_sift.py
+++ w1/dense_sift.py
@@ -1,16 +1,17 @@
+import random
+
 import cv2
 import numpy as np
 
 
 class DenseSift:
-
     step_size: int
 
     def __init__(self, step_size: int):
         self.step_size = step_size
 
     def dense_sift(self, sift, img: np.ndarray):
-        kp = [cv2.KeyPoint(x, y, self.step_size) for y in range(0, img.shape[0], self.step_size)
+        kp = [cv2.KeyPoint(x, y, self.step_size * random.uniform(1, 3)) for y in range(0, img.shape[0], self.step_size)
               for x in range(0, img.shape[1], self.step_size)]
         _, des = sift.compute(img, kp)
 
