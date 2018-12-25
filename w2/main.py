30956d7412ccf478062faec48ed11b69aeb4de6d
Oscar Mañas <oscmansan@gmail.com>
2018-12-25 19:58:40 +0100

    Add cache for SIFT descriptors

--- w2/main.py
+++ w2/main.py
@@ -1,7 +1,9 @@
+import os
 import argparse
 from shutil import rmtree
 from tempfile import mkdtemp
 
+import numpy as np
 from joblib import Memory
 from sklearn.model_selection import GridSearchCV
 from sklearn.pipeline import Pipeline
@@ -14,11 +16,25 @@ from utils.load_data import load_dataset
 from utils.timer import Timer
 
 
-def parse_args():
+def _parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument('--train_path', type=str, default='../data/MIT_split/train')
     parser.add_argument('--test_path', type=str, default='../data/MIT_split/test')
+    parser.add_argument('--cache_path', type=str, default='../data/descriptors')
     return parser.parse_args()
+
+
+def _load_or_compute(filenames, cache_path, step_size=16):
+    cache_file = os.path.join(cache_path, 'descriptors_step_{}.npy'.format(step_size))
+    if os.path.exists(cache_file):
+        descriptors = np.load(cache_file)
+    else:
+        if not os.path.exists(cache_path):
+            os.makedirs(cache_path)
+        sift = DenseSIFT(step_size)
+        descriptors = sift.compute(filenames)
+        np.save(cache_file, descriptors)
+    return descriptors
 
 
 def main(args):
@@ -27,14 +43,15 @@ def main(args):
     test_filenames, test_labels = load_dataset(args.test_path)
 
     # Compute the Dense SIFT descriptors for all the train and test images.
-    sift = DenseSIFT(step_size=16)
     with Timer('Extract train descriptors'):
-        train_descriptors = sift.compute(train_filenames)
+        train_cache_path = os.path.join(args.cache_path, 'train')
+        train_descriptors = _load_or_compute(train_filenames, train_cache_path)
     with Timer('Extract test descriptors'):
-        test_descriptors = sift.compute(test_filenames)
+        test_cache_path = os.path.join(args.cache_path, 'test')
+        test_descriptors = _load_or_compute(test_filenames, test_cache_path)
 
     # Create processing pipeline and run cross-validation.
-    transformer = SpatialPyramid(levels=1)
+    transformer = SpatialPyramid(levels=2)
     scaler = StandardScaler()
     classifier = SVC(C=1, kernel='rbf', gamma=.002)
 
@@ -43,21 +60,21 @@ def main(args):
     pipeline = Pipeline(memory=None,
                         steps=[('transformer', transformer), ('scaler', scaler), ('classifier', classifier)])
     param_grid = {}
-    cv = GridSearchCV(pipeline, param_grid, n_jobs=1, cv=3, refit=True, verbose=2)
+    cv = GridSearchCV(pipeline, param_grid, n_jobs=-1, cv=3, refit=True, verbose=2)
 
-    with Timer('train'):
+    with Timer('Train'):
         cv.fit(train_descriptors, train_labels)
 
-    with Timer('test'):
+    with Timer('Test'):
         accuracy = cv.score(test_descriptors, test_labels)
 
     # TODO print scores
     print(cv.cv_results_)
 
-    print('accuracy: {}'.format(accuracy))
+    print('Accuracy: {}'.format(accuracy))
 
     rmtree(cachedir)
 
 
 if __name__ == '__main__':
-    main(parse_args())
+    main(_parse_args())
