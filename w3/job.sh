49a5ff2de8f994e1b0dba0fb32912b224b4d5166
Jorge López <jorge.lopez@neosentec.com>
2019-01-15 17:58:25 +0100

    Fixing job

--- w3/job.sh
+++ w3/job.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 #SBATCH -n 4 # Number of cores
 #SBATCH --mem 1000 # 2GB solicitados.
-#SBATCH -D /home/grupo06/batch # working directory
+#SBATCH -D /home/grupo06/ # working directory
 #SBATCH -p mhigh,mhigh # or mlow Partition to submit to
 #SBATCH --gres gpu:1 # Para pedir Pascales MAX 8
-#SBATCH -o %x_%u_%j.out # File to which STDOUT will be written
-#SBATCH -e %x_%u_%j.err # File to which STDERR will be written
+#SBATCH -o logs/%x_%u_%j.out # File to which STDOUT will be written
+#SBATCH -e logs/%x_%u_%j.err # File to which STDERR will be written
+
+cd m3-project/w3
 source ../venv/bin/activate
 python mlp_MIT_8_scene.py
