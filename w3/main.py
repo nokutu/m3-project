a778410ab90c1ee85adabc7b8f4de2f4591916f3
albamhp <albaherrerapalacio@gmail.com>
2019-01-16 11:53:27 +0100

    Main changes

--- w3/main.py
+++ w3/main.py
@@ -17,4 +17,55 @@ def parse_args() -> argparse.Namespace:
 
 if __name__ == '__main__':
     args = parse_args()
+    
     model_creation(args.image_size, args.units, args.activation, args.loss, args.optimizer, args.metrics)
+
+    print(model.summary())
+    plot_model(model, to_file='/home/grupo06/work/modelMLP.png', show_shapes=True, show_layer_names=True)
+    print('Done!\n')
+
+    if os.path.exists(file_name):
+        print('WARNING: model file ' + file_name + ' exists and will be overwritten!\n')
+
+    print('Start training...\n')
+
+    # this is the dataset configuration we will use for training
+    # only rescaling
+    train_datagen = ImageDataGenerator(
+        rescale=1. / 255,
+        horizontal_flip=True)
+
+    # this is the dataset configuration we will use for testing:
+    # only rescaling
+    test_datagen = ImageDataGenerator(rescale=1. / 255)
+
+    # this is a generator that will read pictures found in
+    # subfolders of 'data/train', and indefinitely generate
+    # batches of augmented image data
+    train_generator = train_datagen.flow_from_directory(
+        DATASET_DIR + '/train',  # this is the target directory
+        target_size=(IMG_SIZE, IMG_SIZE),  # all images will be resized to IMG_SIZExIMG_SIZE
+        batch_size=BATCH_SIZE,
+        classes=['coast', 'forest', 'highway', 'inside_city', 'mountain', 'Opencountry', 'street', 'tallbuilding'],
+        class_mode='categorical')  # since we use binary_crossentropy loss, we need categorical labels
+
+    # this is a similar generator, for validation data
+    validation_generator = test_datagen.flow_from_directory(
+        DATASET_DIR + '/test',
+        target_size=(IMG_SIZE, IMG_SIZE),
+        batch_size=BATCH_SIZE,
+        classes=['coast', 'forest', 'highway', 'inside_city', 'mountain', 'Opencountry', 'street', 'tallbuilding'],
+        class_mode='categorical')
+
+    history = model.fit_generator(
+        train_generator,
+        steps_per_epoch=1881 // BATCH_SIZE,
+        epochs=50,
+        verbose=2,
+        validation_data=validation_generator,
+        validation_steps=807 // BATCH_SIZE)
+
+    print('Done!\n')
+    print('Saving the model into ' + MODEL_FNAME + ' \n')
+    model.save_weights(MODEL_FNAME)  # always save your weights after training or during training
+    print('Done!\n')
