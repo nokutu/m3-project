f8896948bf2aa58a822f76f61ea11a554205d3fa
albamhp <albaherrerapalacio@gmail.com>
2019-01-16 11:41:31 +0100

    MLP

---
+++ w3/main.py
@@ -0,0 +1,20 @@
+import argparse
+from model.multi_layer_perceptron import model_creation
+
+
+def parse_args() -> argparse.Namespace:
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-d', '--dataset', type=str, default='/home/mcv/datasets/MIT_split')
+    parser.add_argument('-u', '--units', type=int, nargs='+', default=[2048, 1024])
+    parser.add_argument('-a', '--activation', type=str, nargs='+', default=['relu', 'relu'])
+    parser.add_argument('-l', '--loss', type=str, default='categorical_crossentropy')
+    parser.add_argument('-o', '--optimizer', type=str, default='sgd')
+    parser.add_argument('-m', '--metrics', type=str, nargs='+', default=['accuracy'])
+    parser.add_argument('-s', '--image-size', type=int, default=64)
+    parser.add_argument('-n', '--names', type=str, nargs='+')
+    return parser.parse_args()
+
+
+if __name__ == '__main__':
+    args = parse_args()
+    model_creation(args.image_size, args.units, args.activation, args.loss, args.optimizer, args.metrics)
---
+++ w3/model/multi_layer_perceptron.py
@@ -0,0 +1,42 @@
+import os
+from typing import List
+
+from keras.models import Sequential, Model
+from keras.layers import Flatten, Dense, Reshape
+from keras.preprocessing.image import ImageDataGenerator
+from keras.utils import plot_model
+
+
+def _model_layer(model: Sequential, units: List[int], activation: List[str], name: str) -> Model:
+    if len(units) != len(activation):
+        raise Exception('ERROR: feature layer parameters length is not equal!')
+    for i in range(len(activation)):
+        model.add(Dense(units=units[i], activation=activation[i], name=name + str(i)))
+    return model
+
+
+def model_creation(image_size: int, units: List[int], activation: List[str], loss: str, optimizer: str,
+                   metrics: List[str]) -> Model:
+    # Build the Multi Layer Perceptron model
+    model = Sequential()
+    model.add(Reshape((image_size * image_size * 3,), input_shape=(image_size, image_size, 3), name='data'))
+    _model_layer(model, units, activation, 'fc')
+    model.add(Dense(units=8, activation='softmax', name='prob'))
+    _model_compile(model, loss, optimizer, metrics)
+    return model
+
+
+def _model_compile(model: Model, loss: str, optimizer: str, metrics: List[str]) -> Model:
+    model.compile(loss=loss,
+                  optimizer=optimizer,
+                  metrics=metrics)
+    return model
+
+
+def _print_save_model(model: Model, file_name: str):
+    print(model.summary())
+    plot_model(model, to_file='/home/grupo06/work/modelMLP.png', show_shapes=True, show_layer_names=True)
+    print('Done!\n')
+
+    if os.path.exists(file_name):
+        print('WARNING: model file ' + file_name + ' exists and will be overwritten!\n')
