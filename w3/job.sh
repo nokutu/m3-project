35585a2324870b5b9f958d5fe2c92c0820af9f85
Jorge López <jorge.lopez@neosentec.com>
2019-01-17 17:13:31 +0100

    Added more configs

--- w3/job.sh
+++ w3/job.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 #SBATCH --job-name group06-w3
 #SBATCH --ntasks 4
-#SBATCH --mem 8G
+#SBATCH --mem 4G
 #SBATCH --partition mhigh,mlow
 #SBATCH --gres gpu:1
 #SBATCH --chdir /home/grupo06/
 #SBATCH --output logs/%x_%A_%a.out
-#SBATCH --array=0-63
+#SBATCH --array=64-85
 
 source venv/bin/activate
 
