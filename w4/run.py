63beb335ec68fcfde21199893dff11e2003e897e
Oscar Mañas <oscmansan@gmail.com>
2019-01-23 20:22:38 +0100

    Fix tensorboard log

--- w4/run.py
+++ w4/run.py
@@ -10,14 +10,13 @@ from keras import optimizers
 from keras.layers import Dense
 from keras.models import Model
 
-from model.TestCallback import TestCallback
 from utils import get_train_generator, get_validation_generator
 
 
 def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument('index', type=int)
-    parser.add_argument('-d', '--dataset_dir', type=str, default='/home/grupo06/MIT_split')
+    parser.add_argument('-d', '--dataset_dir', type=str, default='/home/mcv/datasets/MIT_split')
     parser.add_argument('-o', '--output_dir', type=str, default='/home/grupo06/work')
     parser.add_argument('-l', '--log_dir', type=str, default='/home/grupo06/logs/tensorboard')
     parser.add_argument('-b', '--batch_size', type=int, default=32)
@@ -34,7 +33,7 @@ def get_config(args: argparse.Namespace):
     losses = ['categorical_crossentropy']
     learning_rates = [0.0001, 0.0005, 0.001, 0.005, 0.01, 0.05, 0.1]
     momenta = [0.0, 0.2, 0.4, 0.6, 0.8, 0.9]
-    lr_fraction_decays = [0, 0.1]
+    decays = [0, 0.1]
 
     config = {
         'batch_size': args.batch_size,
@@ -43,7 +42,7 @@ def get_config(args: argparse.Namespace):
         'loss': np.random.choice(losses),
         'learning_rate': np.random.choice(learning_rates),
         'momentum': np.random.choice(momenta),
-        'decay': np.random.choice(lr_fraction_decays)
+        'decay': np.random.choice(decays)
     }
     return config
 
@@ -55,7 +54,7 @@ def config_to_str(config):
     return '__'.join(s)
 
 
-def build_model(optimizer_name: str, lr: float, decay_fraction: float, momentum: float, loss: str, classes: int,
+def build_model(optimizer: str, lr: float, decay: float, momentum: float, loss: str, classes: int,
                 freeze=True) -> Model:
     base_model = applications.nasnet.NASNetMobile(
         input_shape=None,
@@ -72,12 +71,12 @@ def build_model(optimizer_name: str, lr: float, decay_fraction: float, momentum:
     my_dense = Dense(classes, activation='softmax', name='predictions')
     model = Model(inputs=base_model.input, outputs=my_dense(base_model.layers[-1].output))
 
-    optimizer = optimizers.get(optimizer_name)
+    optimizer = optimizers.get(optimizer)
     K.set_value(optimizer.lr, lr)
     if hasattr(optimizer, 'momentum'):
         K.set_value(optimizer.momentum, momentum)
     if hasattr(optimizer, 'decay'):
-        K.set_value(optimizer.decay, lr * decay_fraction)
+        K.set_value(optimizer.decay, decay)
 
     model.compile(optimizer, loss, metrics=['accuracy'])
 
@@ -97,8 +96,13 @@ def main():
     config = get_config(args)
     print_setup(config)
 
-    model = build_model(optimizer_name=config['optimizer'], lr=config['learning_rate'], decay_fraction=config['decay'],
-                        momentum=config['momentum'], loss=config['loss'], classes=8)
+    model = build_model(
+        optimizer=config['optimizer'],
+        lr=config['learning_rate'],
+        decay=config['decay'],
+        momentum=config['momentum'],
+        loss=config['loss'],
+        classes=8)
     # model.summary()
 
     train_generator = get_train_generator(args.dataset_dir, config['batch_size'])
@@ -107,24 +111,24 @@ def main():
     tb_callback = callbacks.TensorBoard(log_dir=os.path.join(args.log_dir, config_to_str(config)))
     es_callback = callbacks.EarlyStopping(monitor='val_acc', min_delta=0, patience=args.patience, verbose=0,
                                           mode='auto', baseline=None, restore_best_weights=True)
-    test_callback = TestCallback()
 
     history = model.fit_generator(
         train_generator,
         steps_per_epoch=train_generator.samples // train_generator.batch_size,
         epochs=config['epochs'],
         verbose=2,
-        callbacks=[tb_callback, es_callback, test_callback],
+        callbacks=[tb_callback, es_callback],
         validation_data=validation_generator,
         validation_steps=validation_generator.samples // validation_generator.batch_size,
-        workers=4
-    )
+        workers=4)
 
-    print('Completed first training, initiating full nn retrain')
+    # this is a small hack: https://github.com/keras-team/keras/issues/1766
+    last_epoch = len(history.history['loss'])
+
+    print('\nFine-tuning top layers done. Training full network now...\n')
 
     for layer in model.layers:
         layer.trainable = True
-
     model.compile(model.optimizer, model.loss, model.metrics)
 
     history2 = model.fit_generator(
@@ -132,18 +136,18 @@ def main():
         steps_per_epoch=train_generator.samples // train_generator.batch_size,
         epochs=config['epochs'],
         verbose=2,
-        callbacks=[tb_callback, test_callback, es_callback],
+        callbacks=[tb_callback, es_callback],
         validation_data=validation_generator,
         validation_steps=validation_generator.samples // validation_generator.batch_size,
-        workers=4
-    )
+        workers=4,
+        initial_epoch=last_epoch)
 
     model_file = os.path.join(args.output_dir, 'nasnet__{}.h5'.format(config_to_str(config)))
     model.save(model_file)
     print('Model saved to {}'.format(model_file))
 
-    for key in history.history.keys():
-        history.history[key].extend(history2.history[key])
+    for k in history.history.keys():
+        history.history[k].extend(history2.history[k])
 
     history_file = os.path.join(args.output_dir, 'history__{}.pkl'.format(config_to_str(config)))
     with open(history_file, 'wb') as f:
