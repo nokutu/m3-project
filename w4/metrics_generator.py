966c6fe4d6561dce51269bbb4695016bed524a10
albamhp <albaherrerapalacio@gmail.com>
2019-01-23 12:35:32 +0100

    Confusion matrix

--- w4/metrics_generator.py
+++ w4/metrics_generator.py
@@ -3,6 +3,7 @@ import pickle
 
 from keras.engine.saving import load_model
 
+from utils import get_validation_generator
 from utils.metrics import save_accuracy, save_loss, save_confusion_matrix
 
 
@@ -21,6 +22,8 @@ def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument('-m', '--model_file', type=str)
     parser.add_argument('-o', '--output_dir', type=str, default='/home/grupo06/work')
+    parser.add_argument('-d', '--dataset_dir', type=str, default='/home/mcv/datasets/MIT_split')
+    parser.add_argument('-b', '--batch_size', type=int, default=32)
     return parser.parse_args()
 
 
@@ -30,6 +33,11 @@ def main():
     print('Maximum accuracy: ', history.history['val_acc'][-1])
     save_accuracy(history, args.model_file.replace('model', 'accuracy').replace('.h5', '.png'))
     save_loss(history, args.model_file.replace('model', 'loss').replace('.h5', '.png'))
+
+    validation_generator = get_validation_generator(args.dataset_dir, args.batch_size, False)
+    y_pred = model.predict(validation_generator)
+    save_confusion_matrix(validation_generator.classes, y_pred,
+                          args.model_file.replace('model', 'confusion').replace('.h5', '.png'))
 
 
 if __name__ == '__main__':
