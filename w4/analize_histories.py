c7c497e91a2f6c7182f858761ccd511ceb896c9e
Jorge López Fueyo <jorgelopezfueyo@gmail.com>
2019-01-24 10:33:20 +0100

    Dataframe from histories

--- w4/analize_histories.py
+++ w4/analize_histories.py
@@ -2,23 +2,42 @@ import argparse
 import os
 import pickle
 
+import pandas
 
-def load_best_history(output_dir):
-    best_acc = 0
-    best_history = None
-    best_file = None
+from utils.utils import str_to_config
+
+
+def load_dataframe(output_dir):
+    data = []
     for file in os.listdir(output_dir):
         if file.startswith('history'):
             print('Loading training history from {}...'.format(file))
+            config_str = file.replace('history__', '').replace('.pkl', '')
+            config = str_to_config(config_str)
+
             with open(os.path.join(output_dir, file), 'rb') as pickle_file:
                 history = pickle.load(pickle_file)
-                max_val_acc = max(history['val_acc'])
+                best_index = history['val_acc'].index(max(history['val_acc']))
 
-                if max_val_acc > best_acc:
-                    best_acc = max_val_acc
-                    best_history = history
-                    best_file = file
-    return best_history, best_file, best_acc
+                row = [
+                    config['batch_size'],
+                    config['decay'],
+                    config['epochs'],
+                    config['learning_rate'],
+                    config['loss'],
+                    config['momentum'],
+                    config['optimizer'],
+                    config['second_fit_lr_fraction'],
+                    history['val_acc'][best_index],
+                    history['val_loss'][best_index],
+                    history['acc'][best_index],
+                    history['loss'][best_index]
+                ]
+                data.append(row)
+
+    return pandas.DataFrame(data, columns=['batch_size', 'decay', 'epochs', 'learning_rate', 'loss', 'momentum',
+                                                'optimizer', 'second_fit_lr_fraction', 'best_train_acc',
+                                                'best_train_loss', 'best_val_acc', 'best_val_loss'])
 
 
 def parse_args():
@@ -30,9 +49,10 @@ def parse_args():
 
 def main():
     args = parse_args()
-    best_history, best_file, best_acc = load_best_history(args.output_dir)
-    print('Best model: ', best_file)
-    print('Accuracy: ', best_acc)
+    df = load_dataframe(args.output_dir)
+    pandas.set_option('display.max_columns', 500)
+    pandas.set_option('display.width', 200)
+    print(df)
 
 
 if __name__ == '__main__':
--- w4/utils/utils.py
+++ w4/utils/utils.py
@@ -4,3 +4,10 @@ def config_to_str(config):
         s.append('{}={}'.format(k, v))
     return '__'.join(s)
 
+
+def str_to_config(line):
+    d = {}
+    for pair in line.split('__'):
+        k, v = pair.split('=')
+        d[k] = v
+    return d
