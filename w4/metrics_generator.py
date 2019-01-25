35a00a83d39ea18f15f9f51a3e66c1a3c807a974
Jorge López Fueyo <jorgelopezfueyo@gmail.com>
2019-01-25 13:14:06 +0100

    Fixing metrics generator

--- w4/metrics_generator.py
+++ w4/metrics_generator.py
@@ -6,10 +6,11 @@ from keras.engine.saving import load_model
 
 from utils import get_validation_generator
 from utils.metrics import save_accuracy, save_loss, save_confusion_matrix
+from utils.utils import str_to_config
 
 
 def get_model_history(model_file):
-    model = load_model(model_file)
+    model = load_model(model_file, compile=False)
     history_file = model_file.replace('nasnet', 'history').replace('.h5', '.pkl')
     with open(history_file, 'rb') as file:
         history = pickle.load(file)
@@ -19,7 +20,7 @@ def get_model_history(model_file):
 
 def parse_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument('-m', '--model_file', type=str)
+    parser.add_argument('model_file', type=str)
     parser.add_argument('-o', '--output_dir', type=str, default='/home/grupo06/work')
     parser.add_argument('-d', '--dataset_dir', type=str, default='/home/mcv/datasets/MIT_split')
     parser.add_argument('-b', '--batch_size', type=int, default=32)
@@ -29,7 +30,14 @@ def parse_args():
 def main():
     args = parse_args()
     model, history = get_model_history(args.model_file)
-    print('Maximum accuracy: ', history['val_acc'][-1])
+    print('Maximum accuracy: ', max(history['val_acc']))
+
+    config_str = args.model_file.split('/')[-1].replace('nasnet__', '').replace('.h5', '')
+    print(config_str)
+    config = str_to_config(config_str)
+
+    model.compile(config['optimizer'], config['loss'], metrics=['accuracy'])
+
     save_accuracy(history, args.model_file.replace('nasnet', 'accuracy').replace('.h5', '.png'))
     save_loss(history, args.model_file.replace('nasnet', 'loss').replace('.h5', '.png'))
 
